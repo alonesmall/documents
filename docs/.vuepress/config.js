877bbb65715afe0a7174782fcef7da680a75089b
alonesmall <975026871@qq.com>
2019-06-29 10:38:30 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'aloneMall的主页',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Github', link: 'https://github.com/alonesmall' },
+    ],
+    sidebar: 'auto'
+  }
+}
